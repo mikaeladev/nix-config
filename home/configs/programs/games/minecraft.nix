e457f4966581a0a6dede2b06471423de2651bc64
mikaeladev <100416544+mikaeladev@users.noreply.github.com>
2026-01-10 14:37:21 +0000

    add extra prismlauncher config

--- home/configs/programs/games/minecraft.nix
+++ home/configs/programs/games/minecraft.nix
@@ -1,7 +1,10 @@
-{ config, inputs, pkgs, ... }:
+{ config, inputs, lib, pkgs, ... }:
 
 let
   inherit (config.lib.custom) wrapGraphics;
+
+  storageDrive = "${config.home.homeDirectory}/storage";
+  prismStorage = "${storageDrive}/prism";
 in
 
 {
@@ -12,16 +15,37 @@ in
   programs.prismlauncher = {
     enable = true;
     package = wrapGraphics pkgs.prismlauncher;
-    
+
+    icons = [
+      ./assets/fabulously-optimised.png
+      ./assets/java.png
+    ];
+
     theme = {
       icons = "flat_white";
       widgets = "dark";
       cat = "rory";
     };
 
-    icons = [
-      ./assets/fabulously-optimised.png
-      ./assets/java.png
-    ];
+    extraConfig = {
+      CloseAfterLaunch = true;
+
+      ConsoleFont = lib.head config.fonts.fontconfig.defaultFonts.monospace;
+      ConsoleFontSize = 10;
+      ConsoleMaxLines = 10000;
+
+      DownloadsDir = config.xdg.userDirs.download;
+      CentralModsDir = "${prismStorage}/mods";
+      InstanceDir = "${prismStorage}/instances";
+      SkinsDir = "${prismStorage}/skins";
+      IconsDir = "icons";
+      JavaDir = "java";
+
+      NumberOfConcurrentDownloads = 12;
+      NumberOfConcurrentTasks = 16;
+      RequestTimeout = 30;
+
+      MaxMemAlloc = 1024 * 16;
+    };
   };
 }
