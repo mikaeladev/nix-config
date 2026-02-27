09a2f87d7d9a8354a6932d7ac8f0a40be93421d4
mikaeladev <100416544+mikaeladev@users.noreply.github.com>
2026-02-27 21:17:29 +0000

    update zed config

--- home/configs/programs/editors/zed.nix
+++ home/configs/programs/editors/zed.nix
@@ -16,15 +16,18 @@
     mutableUserTasks = false;
 
     userSettings = {
-      # ui and theme
+      # text rendering
       ui_font_family = "SF Pro";
       ui_font_size = 16;
       buffer_font_family = "JetBrains Mono";
       buffer_font_size = 14;
+      text_rendering_mode = "grayscale";
+
+      # ui & theme
       cursor_blink = true;
       terminal.blinking = "on";
-      project_panel.auto_fold_dirs = false;
       minimap.show = "never";
+      project_panel.auto_fold_dirs = false;
       icon_theme = "Base Charmed Icons";
       theme = {
         mode = "dark";
@@ -41,12 +44,14 @@
       linked_edits = true;
       tab_size = 2;
 
-      # file types and languages
+      # file associations
       file_types = {
         json = [ "*.lsj" ];
         jsonc = [ "*.json" ];
         properties = [ ".env.*" ];
       };
+
+      # language servers
       lsp = {
         vtsls.settings.autoUseWorkspaceTsdk = true;
         qml.binary.arguments = [ "-E" ];
@@ -61,22 +66,30 @@
     };
 
     extensions = [
+      # languages
       "astro"
       "basher"
-      "charmed-icons"
       "desktop"
-      "discord-presence"
       "git-firefly"
       "html"
       "ini"
       "log"
+      "lua"
+      "make"
       "neocmake"
       "nix"
-      "one-dark-pro"
       "qml"
       "scheme"
+      "scss"
       "toml"
       "xml"
+
+      # themes
+      "one-dark-pro"
+      "charmed-icons"
+
+      # other bits
+      "discord-presence"
     ];
   };
 }
