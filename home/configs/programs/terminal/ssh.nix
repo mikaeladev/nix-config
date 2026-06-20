2ccae0924abb3c7073783e787779fc971ec9ef86
mikaeladev <mikaeladev@icloud.com>
2026-06-20 23:39:46 +0100

    add auto-ssh-add script

--- home/configs/programs/terminal/ssh.nix
+++ home/configs/programs/terminal/ssh.nix
@@ -1,3 +1,5 @@
+{ lib, pkgs, ... }:
+
 {
   programs.ssh = {
     enable = true;
@@ -16,4 +18,43 @@
       controlPersist = "no";
     };
   };
+
+  home.packages = [
+    (pkgs.writeShellScriptBin "auto-ssh-add" ''
+      set -euo pipefail
+
+      export PATH="${
+        lib.makeBinPath [
+          pkgs.expect
+          pkgs.openssh
+        ]
+      }:$PATH"
+
+      device=''${1:-secrets}
+
+      device_dir="/run/media/$USER/$device"
+      secrets_dir="$device_dir/ssh/passphrases"
+
+      identities=(auth sign)
+
+      if [ ! -d "$device_dir" ]; then
+        echo "device not found: $device"
+        exit 1
+      fi
+
+      if [ ! -d "$secrets_dir" ]; then
+        echo "ssh secrets not found on device: $device"
+        exit 1
+      fi
+
+      for identity in "''${identities[@]}"; do
+        expect <<EOF
+      spawn ssh-add "$HOME/.ssh/''${identity}_id_ed25519"
+      expect "Enter passphrase"
+      send "$(cat "$secrets_dir/$identity-ssh-passphrase.txt")\r"
+      expect eof
+      EOF
+      done
+    '')
+  ];
 }
