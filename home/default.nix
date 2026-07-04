4411be8f3d6e82136c7aea67280c5a28a3b95f60
mikaeladev <mikaeladev@icloud.com>
2026-07-04 11:39:53 +0100

    update nix config

--- home/default.nix
+++ home/default.nix
@@ -1,9 +1,14 @@
 {
   globals,
   inputs,
+  lib,
   pkgs,
   ...
 }:
+
+let
+  inherit (lib) attrNames genAttrs' nameValuePair;
+in
 
 {
   imports = with inputs; [
@@ -20,8 +25,29 @@
 
   nix = {
     package = pkgs.nixVersions.latest;
-    settings.download-buffer-size = 524288000; # 500 MiB
     assumeXdg = true;
+
+    # keep nixpkgs channel in sync with this flake
+    channels = { inherit (inputs) nixpkgs; };
+
+    # keep flake registry in sync with this flake
+    registry = genAttrs' (attrNames inputs) (
+      s: nameValuePair s { flake = inputs.${s}; }
+    );
+
+    gc = {
+      automatic = true;
+      persistent = true;
+      options = "--delete-older-than 7d";
+      dates = "weekly";
+    };
+
+    settings = {
+      download-buffer-size = 524288000; # 500 MiB;
+      warn-dirty = false;
+      max-jobs = 10;
+      cores = 10;
+    };
   };
 
   nixpkgs = {
