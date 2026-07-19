ad330c6eb5e98d423e2169df02a978b5f5943d8d
mikaeladev <mikaeladev@icloud.com>
2026-07-19 08:22:02 +0100

    implement gram settings

--- home/configs/programs/development/gram.nix
+++ home/configs/programs/development/gram.nix
@@ -4,15 +4,153 @@
   programs.gram = {
     enable = true;
 
+    settings = {
+      theme = "One Dark Pro";
+      icon_theme = "Base Charmed Icons";
+
+      ui_font_family = "SF Pro";
+      ui_font_weight = 400;
+      ui_font_size = 14;
+
+      buffer_font_family = "JetBrains Mono";
+      buffer_font_weight = 400;
+      buffer_font_size = 14;
+
+      text_rendering_mode = "grayscale";
+
+      base_keymap = "VSCode";
+
+      restore_on_startup = "last_workspace";
+      when_closing_with_no_tabs = "close_window";
+
+      soft_wrap = "editor_width";
+      preferred_line_length = 80;
+      tab_size = 2;
+
+      format_on_save = "off";
+      autosave.after_delay.milliseconds = 1000;
+
+      debugger.button = false;
+      diagnostics.button = true;
+      global_lsp_settings.button = true;
+      search.button = true;
+
+      project_panel = {
+        button = true;
+        dock = "left";
+        entry_spacing = "comfortable";
+        file_icons = true;
+        folder_icons = true;
+        git_status = true;
+        indent_size = 20;
+        auto_fold_dirs = false;
+        auto_reveal_entries = true;
+        hide_hidden = false;
+        hide_root = true;
+        sort_mode = "directories_first";
+        sort_order = "upper";
+        starts_open = true;
+        sticky_scroll = true;
+      };
+
+      git_panel = {
+        button = true;
+        dock = "left";
+        status_style = "icon";
+        sort_by_path = false;
+      };
+
+      outline_panel.button = false;
+
+      title_bar = {
+        show_branch_name = true;
+        show_branch_status_icon = true;
+        show_project_items = true;
+        show_onboarding_banner = false;
+        show_user_picture = false;
+        show_menus = false;
+      };
+
+      tab_bar = {
+        show = true;
+        show_tab_bar_buttons = false;
+        show_nav_history_buttons = false;
+      };
+
+      status_bar = {
+        active_language_button = true;
+        cursor_position_button = true;
+        line_endings_button = false;
+        active_encoding_button = "non_utf8";
+      };
+
+      tabs = {
+        git_status = true;
+        file_icons = true;
+        show_close_button = "hover";
+        show_diagnostics = "all";
+      };
+
+      terminal = {
+        button = true;
+        blinking = "on";
+        cursor_shape = "block";
+        font_family = "JetBrainsMono Nerd Font";
+        font_weight = 400;
+        font_size = 14;
+      };
+
+      gutter = {
+        min_line_number_digits = 4;
+        line_numbers = true;
+        runnables = false;
+        breakpoints = false;
+        folds = false;
+      };
+
+      scrollbar = {
+        show = "auto";
+        diagnostics = "all";
+        cursors = true;
+        git_diff = true;
+        search_results = true;
+        selected_text = true;
+        selected_symbol = true;
+      };
+
+      minimap.show = "never";
+
+      file_types = {
+        json = [ "*.lsj" ];
+        jsonc = [ "*.json" ];
+        properties = [ ".env.*" ];
+      };
+
+      languages = {
+        Nix = {
+          language_servers = [
+            "nixd"
+            "!nil"
+          ];
+          formatter.external = {
+            command = "nix";
+            arguments = [
+              "fmt"
+              "--"
+              "--stdin"
+              "{buffer_path}"
+            ];
+          };
+        };
+      };
+    };
+
     extensionPackages = with pkgs.zed-extensions; [
       # languages
-      basher
       desktop
       git-firefly
-      html
       ini
       log
-      lua
       make
       neocmake
       qml
--- home/modules/programs/gram.nix
+++ home/modules/programs/gram.nix
@@ -10,6 +10,7 @@ let
     escapeShellArgs
     getName
     getVersion
+    isString
     literalExpression
     mkEnableOption
     mkIf
@@ -18,15 +19,17 @@ let
     optional
     types
     ;
-  inherit (pkgs) runCommand symlinkJoin;
+  inherit (pkgs) formats runCommand symlinkJoin;
+
+  jsonFormat = formats.json { };
 
   cfg = config.programs.gram;
 
-  extensionsDir =
-    if pkgs.stdenv.isDarwin && !config.xdg.enable then
-      "Library/Application Support/Gram/extensions/installed"
+  dataDir =
+    if pkgs.stdenv.isDarwin then
+      "Library/Application Support/Gram"
     else
-      "${config.xdg.dataHome}/gram/extensions/installed";
+      (config.xdg.dataHome + "/gram");
 in
 
 {
@@ -49,12 +52,86 @@ in
       default = [ ];
       example = literalExpression "with pkgs; [ nil ]";
       description = ''
-        List of extra packages to install.
+        List of extra packages available to the binary.
+      '';
+    };
+
+    settings = mkOption {
+      type = with types; either (attrsOf jsonFormat.type) lines;
+      default = { };
+      example = {
+        buffer_font_family = "JetBrains Mono";
+        buffer_font_weight = 400;
+        buffer_font_size = 14;
+      };
+      description = ''
+        Configuration to write to {file}`settings.jsonc`.
+
+        See the Gram [docs] for more information.
+
+        [docs]: https://gram-editor.com/docs/configuring-gram/
+      '';
+    };
+
+    keymaps = mkOption {
+      type = with types; either (listOf jsonFormat.type) lines;
+      default = [ ];
+      example = [
+        {
+          bindings = {
+            ctrl-right = "editor::SelectLargerSyntaxNode";
+            ctrl-left = "editor::SelectSmallerSyntaxNode";
+          };
+        }
+        {
+          context = "ProjectPanel && not_editing";
+          bindings = {
+            "o" = "project_panel::Open";
+          };
+        }
+      ];
+      description = ''
+        List of key bindings to write to {file}`keymap.jsonc`.
+
+        See the Gram [docs] for more information.
+
+        [docs]: https://gram-editor.com/docs/key-bindings/
+      '';
+    };
+
+    debugger = mkOption {
+      type = with types; either (listOf jsonFormat.type) lines;
+      default = [ ];
+      example = [
+        {
+          label = "Example Start debugger config";
+          adapter = "Example adapter name";
+          request = "launch";
+          program = "path_to_program";
+          cwd = "$GRAM_WORKTREE_ROOT";
+        }
+      ];
+      description = ''
+        List of debug configurations to write to {file}`debug.jsonc`.
+
+        See the Gram [docs] for more information.
+
+        [docs]: https://gram-editor.com/docs/debugger/
       '';
     };
   };
 
   config = mkIf cfg.enable {
+    assertions = [
+      {
+        assertion = cfg.extraPackages != [ ] -> cfg.package != null;
+        message = ''
+          The `programs.gram.extraPackages` option requires that `programs.gram.package`
+          not be null in order to be applied.
+        '';
+      }
+    ];
+
     home.packages = optional (cfg.package != null) (
       if cfg.extraPackages == [ ] then
         cfg.package
@@ -72,9 +149,32 @@ in
         })
     );
 
-    home.file.${extensionsDir} = {
+    xdg.configFile = {
+      "gram/settings.jsonc" = mkIf (cfg.settings != { }) (
+        if isString cfg.settings then
+          { text = cfg.settings; }
+        else
+          { source = jsonFormat.generate "settings.jsonc" cfg.settings; }
+      );
+      "gram/keymap.jsonc" = mkIf (cfg.keymaps != [ ]) (
+        if isString cfg.keymaps then
+          { text = cfg.keymaps; }
+        else
+          { source = jsonFormat.generate "keymap.jsonc" cfg.keymaps; }
+      );
+      "gram/debug.jsonc" = mkIf (cfg.debugger != [ ]) (
+        if isString cfg.debugger then
+          { text = cfg.debugger; }
+        else
+          { source = jsonFormat.generate "debug.jsonc" cfg.debugger; }
+      );
+    };
+
+    home.file."${dataDir}/extensions/installed" = {
       recursive = true;
       source = runCommand "gram-extensions" { } ''
+        set -euo pipefail
+
         drvs=(${escapeShellArgs cfg.extensionPackages})
         paths=(/share/gram/extensions /share/zed/extensions)
 
