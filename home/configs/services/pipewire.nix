d643699ecae0fbc18a492c041dbfe4d08966d933
mikaeladev <mikaeladev@icloud.com>
2026-07-26 23:24:17 +0100

    reconfigure pipewire

--- home/configs/services/pipewire.nix
+++ home/configs/services/pipewire.nix
@@ -1,67 +1,117 @@
-let
-  mkRenameRule = { name, device }: {
-    "actions.update-props" = {
-      "alsa.card_name" = name;
-      "alsa.long_card_name" = name;
-      "device.name" = name;
-      "device.description" = name;
-      "node.description" = name;
-      "node.nick" = name;
-    };
-    "matches" = [ { "node.name" = device; } ];
-  };
-in
-
 {
   services.pipewire = {
     enable = true;
+
     configs = {
-      "10-default" = {
+      "10-low-latency" = {
         "context.properties" = {
           "default.clock.rate" = 48000;
-          "default.clock.quantum" = 2048;
 
-          "default.clock.min-quantum" = 1024;
-          "default.clock.max-quantum" = 8192;
+          "default.clock.quantum" = 256;
+          "default.clock.min-quantum" = 128;
+          "default.clock.max-quantum" = 1024;
 
           "default.clock.allowed-rates" = [
+            32000
             44100
             48000
             88200
             96000
+            176400
+            192000
           ];
+        };
+      };
+    };
+
+    pulseConfigs = {
+      "10-low-latency" = {
+        "context.modules" = [
+          {
+            "name" = "libpipewire-module-rt";
+            "args" = {
+              "nice.level" = -20;
+              "rt.prio" = 99;
+            };
+          }
+        ];
+        "pulse.properties" = {
+          "pulse.default.req" = "256/48000";
+          "pulse.min.req" = "256/48000";
+          "pulse.min.quantum" = "256/48000";
+        };
+      };
+    };
+
+    jackConfigs = {
+      "10-low-latency" = {
+        "jack.properties" = {
+          "node.latency" = "256/48000";
+          "node.quantum" = "256/48000";
         };
       };
     };
 
     wireplumber = {
       enable = true;
-      configs = {
-        # give devices friendly names
-        "10-rename-devices" = {
-          "monitor.alsa.rules" = [
-            (mkRenameRule {
-              name = "Razer Headset";
-              device = "alsa_output.pci-0000_0a_00.6.analog-stereo";
-            })
-            (mkRenameRule {
-              name = "Razer Microphone";
-              device = "alsa_input.pci-0000_0a_00.6.analog-stereo";
-            })
-          ];
+      configs =
+        let
+          analogOutput = "alsa_output.pci-0000_0a_00.6.analog-stereo";
+          analogInput = "alsa_input.pci-0000_0a_00.6.analog-stereo";
+        in
+        {
+          "10-low-latency" = {
+            "monitor.alsa.rules" = [
+              {
+                "matches" = [ { "node.name" = analogOutput; } ];
+                "actions"."update-props" = {
+                  "device.profile" = "pro-audio";
+                  "api.alsa.period-size" = 256;
+                  "api.alsa.period-num" = 3;
+                  "audio.rate" = 48000;
+                };
+              }
+            ];
+          };
+
+          "20-disable-suspend" = {
+            "monitor.alsa.rules" = [
+              {
+                "matches" = [ { "node.name" = analogOutput; } ];
+                "actions"."update-props" = {
+                  "session.suspend-timeout-seconds" = 0;
+                };
+              }
+            ];
+          };
+
+          "90-rename-devices" = {
+            "monitor.alsa.rules" =
+              let
+                mkRenameRule = { name, device }: {
+                  "matches" = [ { "node.name" = device; } ];
+                  "actions"."update-props" = {
+                    "alsa.card_name" = name;
+                    "alsa.long_card_name" = name;
+                    "device.name" = name;
+                    "device.description" = name;
+                    "node.nick" = name;
+                    "node.description" = name;
+                  };
+                };
+              in
+              [
+                (mkRenameRule {
+                  name = "Razer Headset";
+                  device = analogOutput;
+                })
+                (mkRenameRule {
+                  name = "Razer Headset";
+                  device = analogInput;
+                })
+              ];
+          };
         };
-        # disable suspend for headset speakers
-        "20-disable-suspend" = {
-          "monitor.alsa.rules" = [
-            {
-              "actions.update-props" = {
-                "session.suspend-timeout-seconds" = 0;
-              };
-              "matches" = [ { "node.name" = "alsa_output.pci-0000_0a_00.6.analog-stereo"; } ];
-            }
-          ];
-        };
-      };
     };
   };
 }
